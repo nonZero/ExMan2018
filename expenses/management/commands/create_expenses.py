ee2168d548447e37c8a9edc6f4c3ddc5c060214f
Udi Oron <udioron@gmail.com>
2018-07-02 16:09:59 +0300

    added creaate expenses command with silly

---
+++ expenses/management/commands/create_expenses.py
@@ -0,0 +1,20 @@
+import random
+import silly
+from django.core.management.base import BaseCommand
+
+from expenses.models import Expense
+
+
+class Command(BaseCommand):
+    help = "Create some silly expenses."
+
+    def add_arguments(self, parser):
+        parser.add_argument('n', type=int, help="Number of expenses to create")
+
+    def handle(self, n, *args, **kwargs):
+        for i in range(n):
+            o = Expense()
+            o.title = silly.a_thing()
+            o.date = silly.datetime().date()
+            o.amount = f"{random.uniform(1, 1000):.2f}"
+            o.save()
